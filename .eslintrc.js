b5cde875ae76b3f32bd5642dc7a0e9bb9f66e385
flowerLang <m15779063830_1@163.com>
2021-07-10 10:47:33 +0800

    fix(other): 修改依赖包，删除jest单元测试

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,7 @@ module.exports = {
   extends: [
     'plugin:vue/essential',
     'airbnb-base',
-    'plugin:prettier/recommended', // 添加 prettier 插件
-    'plugin:jest/recommended'
+    'plugin:prettier/recommended' // 添加 prettier 插件
   ],
   parserOptions: {
     ecmaVersion: 12,
