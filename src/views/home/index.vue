672af6c7cbf1431adedb996c1bd9bcbadd536616
flowerLang <m15779063830_1@163.com>
2021-07-10 10:38:56 +0800

    feat(other): vue3模板项目

---
+++ src/views/home/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="index-page">主页</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'home'
+})
+</script>
+
+<style scoped></style>
